6918710d5510975e3fcdfd7bbd4d24f123806713
makkamadin <59823750+makkamadin@users.noreply.github.com>
2020-04-10 19:57:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ import time
 
 
 def main():
-    previous_time = ''
-    timegm = time.gmtime()
     with TelegramClient(config.session_name, config.api_id, config.api_hash) as client:
         while True:
-            if not previous_time == "{}:{}".format(timegm.tm_hour+3, timegm.tm_min):
+            timee = round(time.time())
+            if timee % 60 == 0:
                 timegm = time.gmtime()
                 current_time = "{}:{}".format(timegm.tm_hour+3, timegm.tm_min)
-                previous_time = current_time
                 generate_image(current_time)
                 image = client.upload_file('time.jpg')
                 client(DeletePhotosRequest(client.get_profile_photos('me')))

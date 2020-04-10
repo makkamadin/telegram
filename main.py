edaf8247283228a13427eb36952b8597003d4e53
makkamadin <59823750+makkamadin@users.noreply.github.com>
2020-04-10 18:29:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import time
 
 def main():
     previous_time = ''
+    timegm = time.gmtime()
     with TelegramClient(config.session_name, config.api_id, config.api_hash) as client:
         while True:
-            if not previous_time == time.strftime("%H:%M"):
-                current_time = time.strftime("%H:%M", time.time()+10800)
+            if not previous_time == "{}:{}".format(timegm.tm_hour+3, timegm.tm_min):
+                timegm = time.gmtime()
+                current_time = "{}:{}".format(timegm.tm_hour+3, timegm.tm_min)
                 previous_time = current_time
                 generate_image(current_time)
                 image = client.upload_file('time.jpg')
